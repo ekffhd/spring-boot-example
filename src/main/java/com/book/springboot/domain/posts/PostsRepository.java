ff57aa938dd4018e1d69322fe39b10ac88fcc289
shelly <shelltlove1@naver.com>
2020-09-13 13:45:58 +0900

    PUT, RESPONSE,UPDATE API

--- src/main/java/com/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/book/springboot/domain/posts/PostsRepository.java
@@ -1,6 +1,6 @@
 package com.book.springboot.domain.posts;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+        import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> {
 }
